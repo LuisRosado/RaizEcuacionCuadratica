7743d0a1b1ab57fa6e19712e5e6dd40988070439
LuisRosado <140114139+LuisRosado@users.noreply.github.com>
2023-07-20 15:36:10 -0600

    Add files via upload

---
+++ Raiz_ecuacion_cuadratica.py
@@ -0,0 +1,34 @@
+'''script en Python que calcule las raices de una ecuación cuadrática. El usuario debe ingresar los términos a, b, c
+y el programa deberá calcular las raíces con la fórmula general y mostrar el valor de ambas raíces'''
+
+
+print('Ingrese los coeficientes de la ecuacion de izquierda a derecha: ')
+
+a = int(input('Ingrese el primer coeficiente: '))
+
+b = int(input('Ingrese el segundo coeficiente: '))
+
+c = int(input('Ingrese el tercer coeficiente: '))
+
+discriminante = (b**2) - (4*a*c)
+
+if discriminante < 0:
+
+    x = (-b + (discriminante ** 0.5 )) / (2 * a)
+
+    y = (-b - (discriminante ** 0.5 )) / (2 * a)
+
+    print('Recordando que i = √ -1, tenemos que las raices son:  ')
+    print('X1 = ', x, '; X2 = ', y)
+
+elif discriminante == 0:
+    x = (-b + (discriminante ** 0.5 )) / (2 * a)
+    print('La solucion unica es: ',x)
+
+else:
+    x = (-b + (discriminante ** 0.5 )) / (2 * a)
+
+    y = (-b - (discriminante ** 0.5)) / (2 * a)
+
+    print('Las Raices son: ')
+    print('X1 = ',x,'; X2 = ',y)
